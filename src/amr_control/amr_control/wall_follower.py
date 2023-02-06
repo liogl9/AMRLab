9717662f64c179d6dab38e1cd9afc540753c27ef
Lionel Güitta <lionelgl99@gmail.com>
2023-02-06 12:54:44 +0100

    Optimized and commented code

--- src/amr_control/amr_control/wall_follower.py
+++ src/amr_control/amr_control/wall_follower.py
@@ -1,5 +1,4 @@
 from typing import List, Tuple
-import time
 from math import pi
 
 
@@ -13,81 +12,95 @@ class WallFollower:
             dt: Sampling period [s].
 
         """
-        self._dt: float = dt
-        self.d_sensors = 0.33 / 2
-        self.w0: float = -0.05
-        self.turning: bool = False
-        self.tr_rgt: bool = False
-        self.tr_lft: bool = False
+        self._dt: float = dt  # Sampling time
 
-        self.rad_turn = 0.0
-        self.kp = 15
-        self.kd = self.kp * 0.6 * self._dt
-        self.wall_ref = 0.35
-        self.last_error = 0.0
-        self.control = 1
+        # Turn variable
+        self.turning: bool = False  # Define generic state of robot turning
+        self.turn_rgt: bool = False  # Define state of robot turning right
+        self.turn_lft: bool = False  # efine state of robot turning left
+        self.rad_turn: float = 0.0  # Cumulative radians turned
+
+        # Control parameters
+        self.kp: float = 9  # Proportional parameter
+        self.kd: float = self.kp * 0.6 * self._dt  # Derivative parameter
+        self.last_error: float = 0.0  # Last error measured
+        self.w0: float = -0.05  # Base angular speed
+        self.wall_ref: float = 0.35  # Wall distance reference
+        self.control: int = 1  # Control type: 0 -> P
+        #                                      1 -> PD
+        # Thresholds for detecting a wall
+        self.front_thr: float = 0.65  # Wall distance for front sensors
+        self.side_thr: float = 0.9  # Wall distance for side sensors
 
     def go_straight(
         self, z_us: List[float], lft_wall: bool, rgt_wall: bool, control: int
     ) -> Tuple[float, float]:
-        v = 0.5
-        if control == 0:
-            if rgt_wall:
-                error = self.wall_ref - z_us[7]
-                w = self.kp * error
-            elif lft_wall:
-                error = self.wall_ref - z_us[0]
-                w = -self.kp * error
-            else:
-                w = self.w0
-        elif control == 1:
-            if rgt_wall:
-                error = self.wall_ref - z_us[7]
-                error_dev = (error - self.last_error) / self._dt
-                self.last_error = error
-                w = self.kp * error + self.kd * error_dev
-            elif lft_wall:
-                error = self.wall_ref - z_us[0]
+        """
+        go_straight: Function to make the robot follow a side wall (priorizing the right one)
+        if detected or keep going straight with a small angular velocity to compensate any possible
+        error if not
+
+        Args:
+            z_us (List[float]): List measure of the ultrasonic sensors
+            lft_wall (bool): True if there is a near left wall
+            rgt_wall (bool): False if there is a near left wall
+            control (int): Control type to be used 0 -> P
+                                                   1 -> PD
+        Returns:
+            v, w Tuple[float, float]: Command of the linear and angular velocities
+        """
+        v = 0.68  # Base linear velocity on straight corridors
+
+        if rgt_wall:
+            # Following right wall
+            error = self.wall_ref - z_us[7]  # Measure error
+            if control == 0:
+                # P control
+                w = self.kp * error  # Implement control
+            elif control == 1:
+                # PD control
+                error_dev = (error - self.last_error) / self._dt  # Calculate derivative
+                self.last_error = error  # Update last error
+                w = self.kp * error + self.kd * error_dev  # Implement control
+        elif lft_wall:
+            # Following left wall
+            error = self.wall_ref - z_us[0]  # Measure error
+            if control == 0:
+                # P control
+                w = -self.kp * error  # Implement control
+            elif control == 1:
+                # PD control
                 error_dev = (error - self.last_error) / self._dt
                 self.last_error = error
                 w = -(self.kp * error + self.kd * error_dev)
-            else:
-                w = self.w0
-        elif control == 2:
-            if rgt_wall:
-                error = self.wall_ref - z_us[7]
-                error_dev = (error - self.last_error) / self._dt
-                self.last_error = error
-                w = self.kp * error + self.kd * error_dev
-            elif lft_wall:
-                error = self.wall_ref - z_us[0]
-                error_dev = (error - self.last_error) / self._dt
-                self.last_error = error
-                w = -(self.kp * error + self.kd * error_dev)
-            else:
-                w = self.w0
+        else:
+            # No walls
+            w = self.w0  # Keep going with a small angular valocity to compensate
 
         return v, w
 
     def turn(self, deg: float, z_w: float, on_point: bool) -> Tuple[float, float]:
         """
-        turn _summary_
-
-        _extended_summary_
+        turn Function to make the robot turn the degrees given on its place or with a small linear
+        velocity
 
         Args:
-            deg (float): _description_
+            deg (float): Degrees to be turned
+            z_w (float): Actual angular speed
+            on_point (bool): Wether to turn on itself (True) or moving forward too (False)
 
         Returns:
-            Tuple[float, float]: _description_
+            v, w Tuple[float, float]: Command of the linear and angular velocities
         """
+        # Define base linear velocity and angular velocity increments respect the base
         if not on_point:
-            v = 0.0
-            inc_w = 1
+            v = 0.2
+            inc_w = 1.3
         else:
             v = 0.0
-            inc_w = 1
+            inc_w = 1.4
 
+        # Check if it needs to turn more or not
         if self.rad_turn < (abs(deg) * pi / 180.0):
             self.rad_turn = self.rad_turn + abs(z_w) * self._dt
             if deg > 0.0:
@@ -95,36 +108,56 @@ class WallFollower:
             else:
                 w = self.w0 - inc_w
         else:
+            # If it has turned the specified degrees go back to base state
             self.rad_turn = 0.0
             self.turning = False
-            self.tr_lft = False
-            self.tr_rgt = False
+            self.turn_lft = False
+            self.turn_rgt = False
             v = 0.0
             w = 0.0
 
         return v, w
 
-    def get_sides(self, z_us: List[float]) -> Tuple[bool, bool, bool, bool]:
+    def choose_turn(self, right: bool, left: bool):
         """
-        get_sides _summary_
-
-        _extended_summary_
+        choose_turn: Function to specify where does the robot have to turn
 
         Args:
-            z_us (List[float]): _description_
+            right (bool): Wether if there is a near wall on the right (True) or not (False)
+            left (bool): Wether if there is a near wall on the left (True) or not (False)
+        """
+        if right and left:
+            self.turn_rgt = True
+            self.turn_lft = True
+        elif right:
+            self.turn_rgt = False
+            self.turn_lft = True
+        elif left:
+            self.turn_lft = False
+            self.turn_rgt = True
+        elif not (right or left):
+            self.turn_lft = False
+            self.turn_rgt = True
+
+    def get_sides(self, z_us: List[float]) -> Tuple[bool, bool, bool, bool]:
+        """
+        get_sides: Check if ther is any wall on the front, the rear or the sides
+
+        Args:
+            z_us (List[float]): List measure of the ultrasonic sensors
 
         Returns:
-            Tuple[bool, bool]: True if it detects a wall
+            left, right, front, rear Tuple[bool, bool, bool, bool]: True if it detects a wall
         """
         left_sns = [z_us[0], z_us[-1]]
         right_sns = [z_us[7], z_us[8]]
         front_sns = [z_us[3], z_us[4]]
         rear_sns = [z_us[11], z_us[12]]
 
-        left = all([x < 0.9 for x in left_sns])
-        right = all([x < 0.9 for x in right_sns])
-        front = all([x < 0.35 for x in front_sns])
-        rear = all([x < 0.35 for x in rear_sns])
+        left = all([x < self.side_thr for x in left_sns])
+        right = all([x < self.side_thr for x in right_sns])
+        front = all([x < self.front_thr for x in front_sns])
+        rear = all([x < 0.65 for x in rear_sns])
 
         return left, right, front, rear
 
@@ -147,63 +180,27 @@ class WallFollower:
         left, right, front, rear = self.get_sides(z_us)
 
         if not front and not self.turning:
+            # If there is no wall on the front side and it is not turning go straight
             v, w = self.go_straight(z_us, left, right, self.control)
         else:
+            # If there is a wall on the front or the robot is turning
             if not self.turning:
-                self.turning = True
-                if right and left:
-                    self.tr_rgt = True
-                    self.tr_lft = True
-                elif right:
-                    self.tr_rgt = True
-                    self.tr_lft = False
-                elif left:
-                    self.tr_lft = True
-                    self.tr_rgt = False
-                elif not (right or left):
-                    self.tr_lft = True
-                    self.tr_rgt = False
-            if self.tr_rgt and self.tr_lft:
-                v, w = self.turn(180, z_w, True)
-            elif self.tr_rgt:
-                v, w = self.turn(90, z_w, False)
-            elif self.tr_lft:
-                v, w = self.turn(-90, z_w, False)
+                # If it wasn't turning but there is a wall on the front
+                self.turning = True  # start the turning state
+                self.choose_turn(right, left)  # define how much to turn and the orientation
+            if self.turn_rgt and self.turn_lft:
+                # If there are walls on both sides
+                v, w = self.turn(170, z_w, True)
+            elif self.turn_lft:
+                # If there are walls on the right
+                v, w = self.turn(70, z_w, False)
+            elif self.turn_rgt:
+                # If there are walls on the left
+                v, w = self.turn(-70, z_w, False)
             else:
                 v = 0.0
                 w = 0.0
 
-        # front_sns_cls = [x < 0.4 for x in z_us[2:6]]
-        # if not (self.turn_left or self.turn_right):
-        #     if all(front_sns_cls):
-        #         v = 0.0
-        #         w = 0.0
-        #         self.turn = True
-        #         left, right = self.get_sides(z_us)
-        #         if not left:
-        #             self.turn_left = True
-        #         else:
-        #             self.turn_right = True
-
-        #     else:
-        #         v, w = self.go_straight(z_us, z_v, z_w)
-        # else:
-        #     self.rad_turn = self.rad_turn + abs(z_w) * self._dt
-        #     self.time = time.time()
-        #     v = 0.0
-        #     if self.rad_turn < 3.14:
-        #         if self.turn_right:
-        #             w = self.w0 - 0.4
-        #         else:
-        #             w = self.w0 + 0.4
-        #     else:
-        #         self.rad_turn = 0
-        #         self.turn_right = False
-        #         self.turn_left = False
-        #         v = 0.0
-        #         w = 0.0
-
         # w>0 izq; w<0 der
-        # w = 0.0
 
         return v, w
